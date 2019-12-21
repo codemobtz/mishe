128ed4b298f08347243469fcdef4796e61e5332f
Othel <Dedtbic>
2019-12-21 10:18:42 +0300

    corrected directory conflict after including pip files to the repo

--- mishe_api/users/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from ..models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'email', 'username', 'phone_number', 'profile_picture']
